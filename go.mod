073cf5f0d576265eac90a5e92e0d7748259c9ab2
Sam Poder <sam.r.poder@gmail.com>
2023-09-27 22:55:30 -0700

    Make a basic TUI

--- go.mod
+++ go.mod
@@ -3,8 +3,16 @@ module the-telephone-game
 go 1.20
 
 require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.6.0 // indirect
 	github.com/iancoleman/strcase v0.0.0-20190422225806-e506e3ef7365 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/rivo/tview v0.0.0-20230927091330-c961b0f0b9c5 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/steebchen/prisma-client-go v0.24.0 // indirect
 	github.com/takuoki/gocase v1.0.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,5 +1,87 @@
 package main
 
-func main() { 
-	
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+	"time"
+)
+
+type Room struct {
+	code         string
+	createdAt    string
+	public	     bool
+}
+
+var rooms = make([]Room, 0)
+
+var pages = tview.NewPages()
+var roomText = tview.NewTextView()
+var app = tview.NewApplication()
+var form = tview.NewForm()
+var roomsList = tview.NewList().ShowSecondaryText(false)
+var flex = tview.NewFlex()
+var text = tview.NewTextView().
+	SetTextColor(tcell.ColorGreen).
+	SetText(" (n) to create a new room \n (j) to join a room \n (q) to quit")
+
+func main() {
+	roomsList.SetSelectedFunc(func(index int, name string, second_name string, shortcut rune) {
+		setRoomText(&rooms[index])
+	})
+
+	flex.SetDirection(tview.FlexRow).
+		AddItem(tview.NewFlex().
+			AddItem(roomsList, 0, 1, true).
+			AddItem(roomText, 0, 4, false), 0, 6, false).
+		AddItem(text, 0, 1, false)
+
+	flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == 113 {
+			app.Stop()
+		} else if event.Rune() == 110 {
+			form.Clear(true)
+			addRoomForm()
+			pages.SwitchToPage("Create Room")
+		}
+		return event
+	})
+
+	pages.AddPage("Menu", flex, true, true)
+	pages.AddPage("Create Room", form, true, false)
+
+	if err := app.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+
+}
+
+func addRoomList() {
+	roomsList.Clear()
+	for index, room := range rooms {
+		roomsList.AddItem(room.code, " ", rune(49+index), nil)
+	}
+}
+
+func addRoomForm() *tview.Form {
+
+	room := Room{}
+
+	form.AddInputField("Code", "", 20, nil, func(code string) {
+		room.code = code
+	})
+
+	form.AddButton("Save", func() {
+		room.createdAt = time.Now().Format(time.RFC3339)
+		rooms = append(rooms, room)
+		addRoomList()
+		pages.SwitchToPage("Menu")
+	})
+
+	return form
+}
+
+func setRoomText(room *Room) {
+	roomText.Clear()
+	text := room.code + "\n" + room.createdAt
+	roomText.SetText(text)
 }
